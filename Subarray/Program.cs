610894b006e6a228fc412c253cb85174b9aed5a6
Karthik Kasubha <karthik.kasubhaforyou@hotmail.com>
2024-06-13 15:41:40 +1200

    Subarray and 2D Matrix till Transpode

---
+++ Subarray/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> Array = new List<int> { 3, 5, 2, 1, 0 };
+        Console.WriteLine("Please enter the length of subarray");
+        int K = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i <= Array.Count - K; i++)
+        {
+            Console.WriteLine(i + ", " + (i + K - 1));
+            Console.WriteLine();
+        }
